06897c846582fdac569d7a29a522dc5a381b685a
solarpy <ishangill2003@gmail.com>
2022-08-12 14:42:18 +0800

    pls commit

---
+++ BIPJ-sharedcopy/Classes/Bitcoin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NBitcoin;
+using NBitcoin.RPC;
+
+namespace BIPJ_sharedcopy.Classes
+{
+    public class Bitcoin
+    {
+        // bitcoin core 
+        // test credentials 
+        public static Network network = Network.TestNet;
+        public static readonly string rpcHost = "127.0.0.1:8332";
+        public static readonly string rpcCredentials = "test123:test123";
+        public static readonly string walletPassPhrase = "wizard";
+        public static readonly int walletUnlockTime = 20;
+    }
+}
---
+++ BIPJ-sharedcopy/Controllers/BitcoinController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BIPJ_sharedcopy.Controllers
+{
+    public class BitcoinController : Controller
+    {
+        // GET: Bitcoin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
